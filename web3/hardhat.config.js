83d17321a90ee89ebb45b482a6f9c788e677a356
Nikolai Mazein <feynmaz@gmail.com>
2024-05-26 23:01:47 +0500

    set up hardhat

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,36 +1,20 @@
-require("@matterlabs/hardhat-zksync-solc");
-
 /** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const RPC_URL = process.env.RPC_URL;
+
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
+  defaultNetwork: "eth_sepolia",
   networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
+    hardhat: {
+      chainId: 11155111,
     },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
-    },
+    eth_sepolia: {
+      url: RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
   },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
+
   solidity: {
     version: "0.8.17",
     settings: {
